5735cc0197071887d88ed080516fac220a7a5218
Shamel Lakin <christlife4u@gmail.com>
2019-10-01 18:08:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+require 'pry'
+class Dog 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name) 
+    @name=name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.print_all
+    self.all.each do |dog_object| 
+    puts dog_object.name
+  end 
+end
+
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def save
+  end
+  
+end
